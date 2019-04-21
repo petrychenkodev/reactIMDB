c5bc4db45c4a8193a975dca2dadfd007fe085d47
Oleksii.Petrychenko <37137346+petrychenkodev@users.noreply.github.com>
2019-04-21 16:49:57 +0300

    Add files via upload

---
+++ precache-manifest.cfb9765c58310235d54cad506261383f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "48d54e81a70df743ddb5",
+    "url": "/reactIMDB/static/js/runtime~main.90e1059f.js"
+  },
+  {
+    "revision": "10f9329db728f49a22e9",
+    "url": "/reactIMDB/static/js/main.a5e77cc0.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/reactIMDB/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "a12b4b124e9e094977e7150b530c34f0",
+    "url": "/reactIMDB/index.html"
+  }
+];
